5fb8f6d0bb333a0ee1099ce38437f18dd0765e4e
chaeyoungwon <epopcy@naver.com>
2024-07-18 12:38:06 +0900

    Feat: 라우터 추가

--- brfly/src/App.js
+++ brfly/src/App.js
@@ -1,5 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Auth from "./components/Auth";
+import Calendar from "./pages/Calendar";
+import News from "./pages/News";
+
 function App() {
-  return <div>Hello world!</div>;
+  <div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth/kakao" element={<Auth />} />
+        <Route path="/calendar" element={<Calendar />} />
+        <Route path="/news" element={<News />} />
+      </Routes>
+    </BrowserRouter>
+  </div>;
 }
 
 export default App;
